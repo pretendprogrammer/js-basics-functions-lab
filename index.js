90c5871aaa2152e23c3ba701f712129c7f997570
Yosef <shmuli5770@gmail.com>
2021-04-07 12:15:32 -0400

    commit tree ff291bc35d02ef94b93fcd3fef780d9957824e31

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(origin) {
+    return Math.abs(origin - 42)
+}
+
+function distanceFromHqInFeet(origin) {
+    return distanceFromHqInBlocks(origin) * 264
+}
+
+function distanceTravelledInFeet(origin, destination) {
+    return Math.abs(origin - destination) * 264
+}
+
+function calculatesFarePrice(origin, destination) {
+    const distance = distanceTravelledInFeet(origin, destination)
+    if (distance <= 400) {
+        return 0
+    } else if (distance <= 2000) {
+        return (distance - 400) * .02
+    } else if (distance <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
